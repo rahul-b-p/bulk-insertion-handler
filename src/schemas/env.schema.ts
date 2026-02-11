f6b4e8604417ee7b6fbb62992aec33c08f4235f2
rahulbp017 <rahul.b@flycatchtech.com>
2026-02-11 18:13:01 +0530

    feat(redis): redis configured

--- src/schemas/env.schema.ts
+++ src/schemas/env.schema.ts
@@ -9,4 +9,7 @@ export const envSchema = z.object({
   LOG_LEVEL: z.enum(['info', 'debug']).default('info'),
   UPLOAD_LIMIT: z.coerce.number().default(1),
   UPLOAD_MAX_SIZE: z.coerce.number().default(10),
+  REDIS_PASSWORD: z.string(),
+  REDIS_PORT: z.coerce.number().default(6379),
+  REDIS_HOST: z.string().default('localhost'),
 });
